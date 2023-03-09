ced1de1e38b65b080621f7b93a55db0ba72bfbfb
Maria Skrabalekova <mariaskrabalekova@gmail.com>
2023-03-09 14:06:25 +0100

    rcv for lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/lists', to: 'lists#index'
+  get '/lists/new', to: 'lists#new', as: :new_list
+  post '/lists', to: 'lists#create'
+  get '/lists/:id', to: 'lists#show', as: :list
 end
